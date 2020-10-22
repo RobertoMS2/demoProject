a2396cc6d2b592566679743f0224c006a0615c4f
robertomartinezsanchez <roberto.martinezsanchez@gmail.com>
2020-10-22 09:36:20 +0200

    Versión 1.0

--- dist/series.b077e5c9.js
+++ dist/series.b077e5c9.js
@@ -54406,7 +54406,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46381" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33269" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
