e3459231d3752d00552a0a134df34c6562caefaa
RobertoMS2 <roberto.martinezsanchez@gmail.com>
2021-01-04 10:03:13 +0100

    RC v1.2

--- src/js/comics/comic.js
+++ src/js/comics/comic.js
@@ -6,10 +6,10 @@ const id = urlParams.get('id');
 
 if (id !== null && id !== '') {
     comicUtils.getComic(id);
-    const elto = document.querySelector('#catHeader');
-    elto.style.display = "none";
+    const elto = document.querySelector('#detHeader');
+    elto.style.display = "block";
 } else {
     comicUtils.getComicsExtended();
-    const elto = document.querySelector('#detHeader');
-    elto.style.display = "none";
+    const elto = document.querySelector('#catHeader');
+    elto.style.display = "block";
 }
--- src/js/series/series.js
+++ src/js/series/series.js
@@ -6,10 +6,10 @@ const id = urlParams.get('id');
 
 if (id !== null && id !== '') {
     seriesUtils.getSerie(id);
-    const elto = document.querySelector('#catHeader');
-    elto.style.display = "none";
+    const elto = document.querySelector('#detHeader');
+    elto.style.display = "block";
 } else {
     seriesUtils.getSeriesExtended();
-    const elto = document.querySelector('#detHeader');
-    elto.style.display = "none";
+    const elto = document.querySelector('#catHeader');
+    elto.style.display = "block";
 }
